a06e3787597af1d8679f90f62b9c40dbcb9e913c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-13 16:26:38 +0400

    Task 3: Search for an Element in a Java Set

---
+++ src/Task3.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task3 {
+    public static void main(String[] args){
+        Set<String> employeesIds = new HashSet<>();
+        employeesIds.add("001");
+        employeesIds.add("002");
+        employeesIds.add("003");
+        employeesIds.add("004");
+        employeesIds.add("005");
+
+        System.out.println(employeesIds);
+        boolean find = employeesIds.contains("001");
+        System.out.println(find);
+    }
+}
