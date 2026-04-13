4ae05f7131a66029fda99ba25b9f68f791f3434d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-13 16:40:01 +0400

    Task 6: Clear All Elements from a Java Set

---
+++ src/Task6.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task6 {
+    public static void main(String[] args){
+        Set<String> userName = new HashSet<>();
+        userName.add("prod1");
+        userName.add("prod2");
+        userName.add("prod3");
+        userName.add("prod4");
+        userName.add("prod5");
+
+        System.out.println(userName);
+        int size = userName.size();
+        System.out.println(size);
+
+        userName.clear();
+        System.out.println(userName);
+
+        int size2 = userName.size();
+        System.out.println(size2);
+
+
+
+    }
+}
