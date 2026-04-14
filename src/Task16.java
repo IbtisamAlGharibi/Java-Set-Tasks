e7d03c7b77fa6490cd65477325ae2374ad46e6d9
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-14 09:57:42 +0400

    Task 16: Store and Manage Unique Student Names in a Java Set

---
+++ src/Task16.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task16 {
+
+    public static void main(String[] args){
+
+        Set<String> studentNames = new HashSet<>();
+        studentNames.add("Ibtisam");
+        studentNames.add("Anfal");
+        studentNames.add("Sara");
+        studentNames.add("Muna");
+        studentNames.add("Noor");
+
+        System.out.println(studentNames);
+        studentNames.add("Noor");
+        System.out.println(studentNames);
+
+        boolean find = studentNames.contains("Ibtisam");
+        System.out.println(find);
+
+        studentNames.remove("Ibtisam");
+        System.out.println(studentNames);
+
+        System.out.println(studentNames.size());
+        studentNames.clear();
+
+        System.out.println(studentNames);
+        System.out.println(studentNames.size());
+    }
+}
