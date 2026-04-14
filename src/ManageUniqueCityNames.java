73ddffe3a8b26107fec823757a36275290b666aa
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-14 15:41:08 +0400

    Task 9: Manage Unique City Names Using a Java Set

--- src/ManageUniqueCityNames.java
+++ src/ManageUniqueCityNames.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class Task9 {
+public class ManageUniqueCityNames {
     public static void main(String[] args){
         Set<String> cityNames = new HashSet<>();
         cityNames.add("Ibri");
