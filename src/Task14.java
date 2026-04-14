6e24ef15139ce1f8eb1eb78b4cf136f71e545e7a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-14 09:52:19 +0400

    Task 14: Store and Manage Unique Website Names in a Java Set

---
+++ src/Task14.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task14 {
+    public static void main(String[] args){
+        Set<String> websiteName = new HashSet<>();
+        websiteName.add("Watts");
+        websiteName.add("Insta");
+        websiteName.add("X");
+        websiteName.add("Teams");
+        websiteName.add("Clickup");
+
+        System.out.println(websiteName);
+        websiteName.add("Slack");
+        System.out.println(websiteName);
+
+        boolean find = websiteName.contains("Teams");
+        System.out.println(find);
+
+        websiteName.remove("Teams");
+        System.out.println(websiteName);
+
+        System.out.println(websiteName.size());
+        websiteName.clear();
+
+        System.out.println(websiteName);
+        System.out.println(websiteName.size());
+
+
+    }
+
+}
