fe6c9a34a939a2bbdefa3c927e95b84546a1243c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-14 15:55:41 +0400

    Task 21: Store and Manage Unique Car Names in a Java Set

--- src/StoreandManageUniqueCarNames.java
+++ src/StoreandManageUniqueCarNames.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class Task21 {
+public class StoreandManageUniqueCarNames {
     public static void main(String[] args){
 
         Set<String> carNames = new HashSet<>();
