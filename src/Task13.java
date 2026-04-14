334c95a8f2b3a5126f2783da823dad0ca5f1fd3a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-14 09:50:11 +0400

    Task 13: Store and Manage Unique Movie Names in a Java Set

---
+++ src/Task13.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task13 {
+    public static void main(String[] args){
+        Set<String> movieNames = new HashSet<>();
+        movieNames.add("Harry Potter");
+        movieNames.add("The old man");
+        movieNames.add("Animal Farm");
+        movieNames.add("The Secret Garden");
+        movieNames.add("Matilda");
+
+        System.out.println(movieNames);
+        movieNames.add("The Giver");
+        System.out.println(movieNames);
+
+        boolean find = movieNames.contains("Matilda");
+        System.out.println(find);
+
+        movieNames.remove("Matilda");
+        System.out.println(movieNames);
+
+        System.out.println(movieNames.size());
+        movieNames.clear();
+
+        System.out.println(movieNames);
+        System.out.println(movieNames.size());
+
+
+    }
+
+}
