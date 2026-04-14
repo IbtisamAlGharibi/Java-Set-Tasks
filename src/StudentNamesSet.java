bc3270c96656caac556f5247b0eaf847dc225008
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-14 15:40:00 +0400

    Task 8: Create a Java Program Using All Basic Set Methods

--- src/StudentNamesSet.java
+++ src/StudentNamesSet.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class Task8 {
+public class StudentNamesSet {
     public static void main(String[] args){
 
         Set<String> studentNames = new HashSet<>();
