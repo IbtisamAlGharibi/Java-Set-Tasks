5af89a0a5ec9cba4d7526de7e727cb9d86972169
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-14 09:20:27 +0400

    Task 9: Manage Unique City Names Using a Java Set

---
+++ src/Task9.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task9 {
+    public static void main(String[] args){
+        Set<String> cityNames = new HashSet<>();
+        cityNames.add("Ibri");
+        cityNames.add("Sohar");
+        cityNames.add("Muscat");
+        cityNames.add("Nizwa");
+        cityNames.add("Salalah");
+
+        System.out.println(cityNames);
+        cityNames.add("Sur");
+        System.out.println(cityNames);
+
+        boolean find = cityNames.contains("Ibri");
+        System.out.println(find);
+
+        cityNames.remove("Sur");
+        System.out.println(cityNames);
+
+        System.out.println(cityNames.size());
+        cityNames.clear();
+
+        System.out.println(cityNames);
+        System.out.println(cityNames.size());
+
+    }
+}
