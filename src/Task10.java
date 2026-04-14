ae033b7833fe16ea1f785311b1077ea4b9f174f2
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-14 09:33:54 +0400

    Task 10: Store and Manage Unique Course Names in a Java Set

---
+++ src/Task10.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task10 {
+    public static void main(String[] args){
+        Set<String> courseNames = new HashSet<>();
+        courseNames.add("IT");
+        courseNames.add("HR");
+        courseNames.add("DM");
+        courseNames.add("SW");
+        courseNames.add("NW");
+
+        System.out.println(courseNames);
+        courseNames.add("HSC");
+        System.out.println(courseNames);
+
+        boolean find = courseNames.contains("IT");
+        System.out.println(find);
+
+        courseNames.remove("HSC");
+        System.out.println(courseNames);
+
+        System.out.println(courseNames.size());
+        courseNames.clear();
+
+        System.out.println(courseNames);
+        System.out.println(courseNames.size());
+
+
+    }
+}
