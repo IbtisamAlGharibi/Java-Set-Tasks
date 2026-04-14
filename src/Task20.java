a31dd4fe6384e5299f7840e152239fc592743b70
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-14 10:23:31 +0400

    Task 20: Store and Manage Unique Subject Names in a Java Set

---
+++ src/Task20.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task20 {
+
+    public static void main(String[] args){
+
+        Set<String> subjectNames = new HashSet<>();
+        subjectNames.add("Math");
+        subjectNames.add("Science");
+        subjectNames.add("English");
+        subjectNames.add("History");
+        subjectNames.add("Computer");
+
+        System.out.println(subjectNames);
+        subjectNames.add("Sport");
+        System.out.println(subjectNames);
+
+        subjectNames.add("Sport");
+        System.out.println(subjectNames);
+
+        boolean find = subjectNames.contains("Computer");
+        System.out.println(find);
+
+        boolean find2 = subjectNames.contains("History");
+        System.out.println(find2);
+
+        subjectNames.remove("History");
+        System.out.println(subjectNames);
+
+        System.out.println(subjectNames.size());
+        subjectNames.add("Math2");
+        System.out.println(subjectNames);
+
+        System.out.println(subjectNames.size());
+        subjectNames.clear();
+
+        System.out.println(subjectNames);
+        System.out.println(subjectNames.size());
+        System.out.println("The set now contains zero elements " + subjectNames);
+
+    }
+}
