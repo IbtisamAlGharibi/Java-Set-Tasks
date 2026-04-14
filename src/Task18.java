2c66115dcb50ed0eb38a9a6e4dd3589c069e4e8f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-14 10:12:32 +0400

    Task 18: Store and Manage Unique City Names in a Java Set

---
+++ src/Task18.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task18 {
+    public static void main(String[] args){
+        Set<String> cityNames = new HashSet<>();
+        cityNames.add("Ibri");
+        cityNames.add("Sohar");
+        cityNames.add("Muscat");
+        cityNames.add("Nizwa");
+        cityNames.add("Salalah");
+
+        System.out.println(cityNames);
+        cityNames.add("Sur");
+        System.out.println(cityNames);
+
+        cityNames.add("Sur");
+        System.out.println(cityNames);
+
+        boolean find = cityNames.contains("Ibri");
+        System.out.println(find);
+
+        boolean find2 = cityNames.contains("Sur");
+        System.out.println(find2);
+
+        cityNames.remove("Sur");
+        System.out.println(cityNames);
+
+        cityNames.add("Musandam");
+        System.out.println(cityNames);
+
+        System.out.println(cityNames.size());
+        cityNames.clear();
+
+        System.out.println("The set now contains zero elements " + cityNames);
+
+    }
+}
